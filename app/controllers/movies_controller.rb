a455525b030425a484ff048c90fad01668c8618c
Mike Wazowski <netlest@hotmail.com>
2013-04-20 05:44:59 -0700

    hw3-1 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,13 +20,14 @@ class MoviesController < ApplicationController
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
+    if @selected_ratings.is_a?(Hash) then @selected_ratings = @selected_ratings.keys end
     
     if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
       session[:sort] = sort
       session[:ratings] = @selected_ratings
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
-    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
+    @movies = Movie.find_all_by_rating(@selected_ratings, ordering)
   end
 
   def new
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
 
-Given /the following movies exist/ do |movies_table|
+Given /the following movies exist:$/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+      title = movie[:title]
+      rating = movie[:rating]
+      release_date = movie[:release_date]
+      Movie.create!(:title => title, :rating => rating, :release_date => release_date)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -19,7 +21,7 @@ end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+#  "When I check the following ratiUnimplemented (MiniTest::Assertion)ngs: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
